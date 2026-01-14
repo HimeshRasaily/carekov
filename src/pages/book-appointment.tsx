cb363360f899629f325573ebe516d254304009d7
HimeshRasaily <himeshrasaily010@gmail.com>
2026-01-14 17:57:29 +0530

    Fix incorrect component import paths causing build failure

--- src/pages/book-appointment.tsx
+++ src/pages/book-appointment.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import Head from "next/head";
-import Header from "../../components/Header";
-import Footer from "../../components/Footer";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 
